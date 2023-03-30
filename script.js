779d64e324da2d706914348d6e25979fc8ff2909
VellyD <viddvd3@gmail.com>
2023-03-30 21:02:55 +0300

    Adding the dynamic content

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const quoteContainer = document.getElementById("container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
+let apiQuotes = [];
+
+function generateQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author || "Unknown";
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+async function getQuotes() {
+  const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    generateQuote();
+  } catch (error) {
+    // Handle error
+    alert(error);
+  }
+}
+
+getQuotes();
