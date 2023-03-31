5df8010b1b865ac65e8c1853c16de152988804b5
VellyD <viddvd3@gmail.com>
2023-03-31 16:51:40 +0300

    adding Loader for initial fetch.

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function completed() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 function generateQuote() {
+  loading();
+
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  authorText.textContent = quote.author || "Unknown";
 
   if (quote.text.length > 120) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
+
+  authorText.textContent = quote.author || "Unknown";
   quoteText.textContent = quote.text;
+  completed();
 }
 
 async function getQuotes() {
+  loading();
   const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
 
   try {
